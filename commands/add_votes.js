eac59b6dd709612e5c192f99dafce43b7a5d9ed4
TwnKey <feitaishi45@gmail.com>
2026-04-30 20:39:58 +0200

    corrigé add vote manuel temporaire

--- commands/add_votes.js
+++ commands/add_votes.js
@@ -1,5 +1,8 @@
 const db = require('../utils/db.js');
 const ROLES_AUTORISES = ["1306002617725353997", "1020694737725956106"];
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const SALON_VOTE_ID = "1492972991418732685";
+const SALON_READONLY_ID = "1493171302624657428";
 
 module.exports = {
     async execute(interaction) {
@@ -7,13 +10,13 @@ module.exports = {
         if (!estAutorise) return interaction.reply({ content: "🛑 Accès refusé.", ephemeral: true });
 
         const targetChannel = await interaction.client.channels.fetch(SALON_READONLY_ID);
-        mission = db.prepare(`SELECT sheet_id, ligne FROM mission_actuelle WHERE id = 1`).get();
-        propositions = db.prepare(`SELECT message_id FROM propositions WHERE (sheet_id, ligne) = (?, ?)`).all(mission.sheet_id, mission.ligne);
+        const mission = db.prepare(`SELECT sheet_id, ligne FROM mission_actuelle WHERE id = 1`).get();
+        const propositions = db.prepare(`SELECT message_id FROM propositions WHERE (sheet_id, ligne) = (?, ?)`).all(mission.sheet_id, mission.ligne);
         const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder().setCustomId('upvote').setStyle(ButtonStyle.Success).setLabel('👍')
         );
 
-        for (proposition of propositions) {
+        for (const proposition of propositions) {
             try {
                 const message = await targetChannel.messages.fetch(proposition.message_id);
                 await message.edit({ components: [row] });
