1b40f2b747410790b54ef594f2fd223d4e0ed2aa
TwnKey <feitaishi45@gmail.com>
2026-05-04 21:32:53 +0200

    tentative en pur local

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 // Création du pool de connexions
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, // ou l'IP de ton serveur MySQL
+    host: '127.0.0.1', // On force l'IPv4 ici
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: 3306, // Port standard interne
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
